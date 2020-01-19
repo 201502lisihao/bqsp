50b8d93f9d3b99106b51df14a07d035bc2f9e314
201502lisihao <735407073@qq.com>
2020-01-19 13:51:12 +0800

    update

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -9,7 +9,7 @@ $this->title = '管理后台-小田新闻网';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>石家庄伊赛牛肉+管理后台</h1>
+        <h1>石家庄伊赛牛肉+</h1>
 
         <p class="lead">http://bqsp.qianzhuli.top</p>
 <!--        <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>-->
